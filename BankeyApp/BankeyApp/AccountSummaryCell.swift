c6abab0e714049eea9fcc380e9d9788727c1c68e
Jim Sweeney <owner@Owners-MacBook-Air.local>
2023-01-07 19:30:12 -0500

    Chevron Image added to account summary cell

--- BankeyApp/BankeyApp/AccountSummaryCell.swift
+++ BankeyApp/BankeyApp/AccountSummaryCell.swift
@@ -14,6 +14,12 @@ class AccountSummaryCell: UITableViewCell {
     let underLineView = UIView ()
     let nameLabel = UILabel()
     
+    let balanceStackView = UIStackView()
+    let balanceLabel = UILabel()
+    let balanceAmountLabel = UILabel()
+    
+    let chevronImageView = UIImageView()
+    
     static let reuseID = "AccountSummaryCell"
     static let rowHieght: CGFloat = 100
     
@@ -42,10 +48,34 @@ extension AccountSummaryCell {
         nameLabel.font = UIFont.preferredFont(forTextStyle: .body)
         nameLabel.text = "Account name"
         
+        balanceStackView.translatesAutoresizingMaskIntoConstraints = false
+        balanceStackView.axis = .vertical
+        balanceStackView.spacing = 0
+
+        balanceLabel.translatesAutoresizingMaskIntoConstraints = false
+        balanceLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        balanceLabel.textAlignment = .right
+        balanceLabel.text = "Some balance"
+
+        balanceAmountLabel.translatesAutoresizingMaskIntoConstraints = false
+        balanceAmountLabel.textAlignment = .right
+        balanceAmountLabel.text = "$37,373,737.37"
+        
+        chevronImageView.translatesAutoresizingMaskIntoConstraints = false
+        let chevronImage = UIImage(systemName: "chevron.right")!.withTintColor(appColor, renderingMode: .alwaysOriginal)
+        chevronImageView.image = chevronImage
+        
         contentView.addSubview(typeLabel)
         contentView.addSubview(underLineView)
         contentView.addSubview(nameLabel)
+        
+        balanceStackView.addArrangedSubview(balanceLabel)
+        balanceStackView.addArrangedSubview(balanceAmountLabel)
+        
+        contentView.addSubview(balanceStackView)
+        contentView.addSubview(chevronImageView)
     }
+    
     private func layout() {
         NSLayoutConstraint.activate([
             typeLabel.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 2),
@@ -56,6 +86,11 @@ extension AccountSummaryCell {
             underLineView.heightAnchor.constraint(equalToConstant: 4),
             nameLabel.topAnchor.constraint(equalToSystemSpacingBelow: underLineView.bottomAnchor, multiplier: 2),
             nameLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            balanceStackView.topAnchor.constraint(equalToSystemSpacingBelow: underLineView.bottomAnchor, multiplier: 0),
+            balanceStackView.leadingAnchor.constraint(equalTo: nameLabel.trailingAnchor, constant: 4),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: balanceStackView.trailingAnchor, multiplier: 4),
+            chevronImageView.topAnchor.constraint(equalToSystemSpacingBelow: underLineView.bottomAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: chevronImageView.trailingAnchor, multiplier: 1)
         ])
     }
 }
